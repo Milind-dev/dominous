d2d2bc582d7f2d6c71591ddc34c58928e9957a43
Milind-dev <milinddev1011@gmail.com>
2022-04-08 03:05:06 +0530

    design

--- myapp/src/components/Pizza.js
+++ myapp/src/components/Pizza.js
@@ -4,13 +4,13 @@ function Pizza({pizza}){
 	const [quantity,setquantity] = useState(1);
 	const [varient,setVarient] = useState('small');
 	return(
-			<div> 
+			<div style={{margin:"100px"}} className="shadow-lg p-1 mb-5 bg-white rounded"> 
 				<h1> {pizza.name}</h1>
 				<img src = {pizza.image} className="img-fluid"  style={{height:"200px"}} alt="pizza piece"/>
 				<div className="flex-container"> 
                   	<div className="w-100">
                   		<p> Varient </p>
-                  		<select value={varient} onChange={(e)=>setVarient(e.target.value)}>
+                  		<select className="form-control" value={varient} onChange={(e)=>setVarient(e.target.value)}>
                   			{ pizza.varients.map(varient=>{
                   					return <option value={varient}> {varient} </option>
                   				})
@@ -21,7 +21,7 @@ function Pizza({pizza}){
 
                   	<div className="w-100"> 
                   		<p> Quantity</p>
-                  		<select value={quantity} onChange={(e)=>{setquantity(e.target.value)}}> 
+                  		<select className="form-control" value={quantity} onChange={(e)=>{setquantity(e.target.value)}}> 
                   			{/*{ pizza.price.map(price=>)}*/}
 	                  		{[...Array(10).keys()].map((x,i)=>{
 	                  			return <option value={i+1}> {i+1} </option>
@@ -31,10 +31,13 @@ function Pizza({pizza}){
 
 
                   	<div className="flex-container"> 
-                  		<div> 
-                  			<h1> Price : {pizza.prices[0][varient] * quantity}</h1>
+                  		<div className="w-100 m-1"> 
+                  			<h1 className="mt-1"> Price  {pizza.prices[0][varient] * quantity} Rs/-</h1>
                   		</div>
-                  		<div> </div>
+                  		<div className="w-100 m-1">
+                  			<button className="btn"> Add Cart </button>
+                  		 </div>
+
                   		<div> </div>
                   	</div>
 
